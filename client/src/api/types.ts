ec4dfc90b93995e4b2c0171a70e59b3cdfc23036
mrunalshri <mrunalyetekar@gmail.com>
2023-02-26 21:45:33 +0000

    added interfaces for PostRequest & PostResponse in clinet/types.ts

--- client/src/api/types.ts
+++ client/src/api/types.ts
@@ -1,13 +1,16 @@
-export type Posts = {
-	postFound: Boolean;
-	id: string;
-	title: string;
-	text: string;
-	author: Users;
-};
-
 export type Users = {
 	id: string;
 	name: string;
 	creationDate: string;
 };
+
+export interface PostRequest {
+	title: string;
+	text: string;
+}
+
+export interface PostResponse extends PostRequest {
+	postFound: Boolean;
+	id: string;
+	author: Users;
+}
